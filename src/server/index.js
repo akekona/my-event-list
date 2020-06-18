dee056e32d5c27512a26b710552957aee9e75c46
Ashley Kekona <56838258+akekona8@users.noreply.github.com>
2020-06-18 05:00:19 -1000

    updated server index

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 (async () => {
   try {
     console.log("Running migrations");
-    return db.migrate.latest().then(() => {
+    return db.migrate.latest({ directory: "src/db/migrations/" }).then(() => {
       app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
     });
   } catch (err) {
