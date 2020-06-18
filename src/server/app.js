a6c4eb26841e26cb9cdacf8bb9fda35fe93711af
Ashley Kekona <56838258+akekona8@users.noreply.github.com>
2020-06-17 18:34:17 -1000

    implement api post requests

--- src/server/app.js
+++ src/server/app.js
@@ -12,6 +12,22 @@ const port = process.env.VUE_APP_PORT;
 app.use(bodyParser.json());
 app.use(cors());
 
+async function validateUsername(username) {
+  try {
+    console.log("checking username");
+    const userList = db
+      .select("username")
+      .table("users")
+      .where("username", username);
+    return userList.then((userList) => {
+      console.log(userList);
+      return userList;
+    });
+  } catch (err) {
+    console.error("Error validating username", err);
+  }
+}
+
 //endpoints
 app.get("/api/users/", async (req, res) => {
   try {
@@ -58,6 +74,50 @@ app.get("/api/events/:id/", async (req, res) => {
   } catch (err) {
     res.sendStatus(400);
     console.error("Error loading user data");
+  }
+});
+
+app.post("/api/events/:id/", async (req, res) => {
+  try {
+    const newEvent = req.body;
+    const userID = req.params.id;
+    const userEvents = await db("events").insert({
+      user_id: userID,
+      event_name: newEvent.eventName,
+      date: newEvent.date,
+      time: newEvent.time,
+      location: newEvent.location,
+    });
+    res.send(userEvents);
+  } catch (err) {
+    res.sendStatus(400);
+    console.error("Error adding new event", err);
+  }
+});
+
+app.post("/api/users/", async (req, res) => {
+  try {
+    const newUser = req.body;
+    console.log("server post", newUser);
+    validateUsername(newUser.username).then((userList) => {
+      if (!userList || userList.length === 0) {
+        const user = db("users").insert({
+          first_name: newUser.firstName,
+          last_name: newUser.lastName,
+          username: newUser.username,
+          password: newUser.password,
+        });
+        return user.then((user) => {
+          res.send(user);
+        });
+      } else {
+        console.error("Username already exists");
+        res.sendStatus(409);
+      }
+    });
+  } catch (err) {
+    res.sendStatus(400);
+    console.error("Error adding new user", err);
   }
 });
 
